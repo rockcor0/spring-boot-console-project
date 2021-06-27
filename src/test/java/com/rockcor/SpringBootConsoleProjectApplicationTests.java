cfcf2a52632f7879dc5d70d67ae03139daada45b
Ricardo Delgado <ridel007@gmail.com>
2021-06-26 19:54:50 -0500

    first

---
+++ src/test/java/com/rockcor/SpringBootConsoleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockcor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConsoleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
